50eaf4ef772ab3e007ce6ccb11abf2546610520d
svredenburg98 <66885962+svredenburg98@users.noreply.github.com>
2020-11-19 18:46:01 -0600

    finished demogrtaphic info card

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -26,6 +26,7 @@ idChoice = d3.select("#selDataset").node().value;
 d3.json("samples.json").then((data) => {
 
     samples = data.samples;
+    metas = data.metadata;
 
     var dataChoice
     var samp;
@@ -38,17 +39,18 @@ d3.json("samples.json").then((data) => {
     console.log(dataChoice);
 
     var trace1 = {
-        x: samples[dataChoice].otu_labels,
+        x: samples[dataChoice].otu_ids.toString(),
         y: samples[dataChoice].sample_values,
+        text: samples[dataChoice].otu_labels,
         type: "bar"
     };
 
     var data1 = [trace1];
 
     var layout = {
-        title: "test",
-        xaxis: { title: "test1" },
-        yaxis: { title: "test2" }
+        title: "Bacteria in Sample",
+        xaxis: { title: "Bacteria ID" },
+        yaxis: { title: "Number of Bacteria" }
     };
 
     Plotly.newPlot("bar", data1, layout);
@@ -57,6 +59,8 @@ d3.json("samples.json").then((data) => {
     var trace2 = {
         x: samples[dataChoice].otu_ids,
         y: samples[dataChoice].sample_values,
+        text: samples[dataChoice].otu_labels,
+        color: samples[dataChoice].otu_ids,
         mode: 'markers',
         marker: {
         size: samples[dataChoice].sample_values
@@ -71,5 +75,62 @@ d3.json("samples.json").then((data) => {
       };
       
       Plotly.newPlot('bubble', data2, layout2);
+
+      var meta;
+        for (meta = 0; meta < metas.length; meta++) {
+            if (metas[meta].id.toString() === idChoice) {
+            metaChoice = meta;
+            }
+        }
+        
+        //creates and replaces list elements every time a new dataset is chosen
+        var age = document.createTextNode("AGE: " + data.metadata[metaChoice].age);
+        var bbtype = document.createTextNode("BBTYPE: " + data.metadata[metaChoice].bbtype);
+        var ethnicity = document.createTextNode("ETHNICITY: " + data.metadata[metaChoice].ethnicity);
+        var gender = document.createTextNode("GENDER: " + data.metadata[metaChoice].gender);
+        var location = document.createTextNode("LOCATION: " + data.metadata[metaChoice].location);
+        var wfreq = document.createTextNode("WFREQ: " + data.metadata[metaChoice].wfreq);
+        var id = document.createTextNode("SAMPLE: " + data.metadata[metaChoice].id);
+        
+        var listAge = document.createElement("li");
+        listAge.setAttribute("id", "age");
+        var listBbtype = document.createElement("li");
+        listBbtype.setAttribute("id", "bbtype");
+        var listEthnicity = document.createElement("li");
+        listEthnicity.setAttribute("id", "ethnicity");
+        var listGender = document.createElement("li");
+        listGender.setAttribute("id", "gender");
+        var listLocation = document.createElement("li");
+        listLocation.setAttribute("id", "location");
+        var listWfreq = document.createElement("li");
+        listWfreq.setAttribute("id", "wfreq");
+        var listId = document.createElement("li");
+        listId.setAttribute("id", "id");
+
+        listAge.appendChild(age);
+        listBbtype.appendChild(bbtype);
+        listEthnicity.appendChild(ethnicity);
+        listGender.appendChild(gender);
+        listLocation.appendChild(location);
+        listWfreq.appendChild(wfreq);
+        listId.appendChild(id);
+
+        var list = document.getElementById("meta-list");
+
+        childAge = document.getElementById("age");
+        childBbtype = document.getElementById("bbtype");
+        childEthnicity = document.getElementById("ethnicity");
+        childGender = document.getElementById("gender");
+        childLocation = document.getElementById("location");
+        childWfreq = document.getElementById("wfreq");
+        childId = document.getElementById("id");
+
+        list.replaceChild(listAge, childAge);
+        list.replaceChild(listBbtype, childBbtype);
+        list.replaceChild(listEthnicity, childEthnicity);
+        list.replaceChild(listGender, childGender);
+        list.replaceChild(listLocation, childLocation);
+        list.replaceChild(listWfreq, childWfreq);
+        list.replaceChild(listId, childId);
   });
 }
